85ebb9aaf0e649f02dd273392f1c31676ae40c3b
Elshadai <elshadaikassutegegn@gmail.com>
2021-06-13 09:35:30 +0300

    Practical 3 finished

---
+++ Lab_08/fork2.c
@@ -0,0 +1,22 @@
+#include<unistd.h>
+#include<stdio.h>
+#include<sys/types.h>
+void main(){
+    pid_t pid = fork();
+    int x = 25;
+    if(pid == 0){
+        x+=5;
+        printf("\nChild! process id : %d\n",getpid());
+        printf("Child! parent process id : %d\n",getppid());
+        printf("Child! parent process id : %d\n",getppid());
+        printf("the value of x is : %d\n",x);
+    }
+    else{
+        x-=5;
+        printf("Parent! process id : %d\n",getpid());
+        printf("Parent! parent process id : %d\n",getppid());
+        printf("the value of x is : %d\n",x);
+        sleep(10);
+    }
+    printf("Good bye from process with id : %d\n",getpid());
+}
